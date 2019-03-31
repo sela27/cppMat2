e55684ed2843d465041941c4c786b05a276d118f
sela27 <sela@goldenberg.xyz>
2019-03-31 19:39:57 +0300

    more Tests

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -17,7 +17,8 @@ int main() {
   threetree.insert(7);
   threetree.insert(3);
   
-  ariel::Tree mytree;  
+  ariel::Tree mytree;
+  ariel::Tree bigtree;  
 
   badkan::TestCase tc("Binary tree");
   tc
@@ -43,20 +44,59 @@ int main() {
 
 
 	tc.CHECK_OK(mytree.insert(-2)).
+	CHECK_EQUAL(mytree.root(),-2).
       CHECK_THROWS(mytree.insert(-2)).
       CHECK_EQUAL(mytree.size(),1).
       CHECK_THROWS(mytree.parent(-2)).
       CHECK_OK(mytree.insert(-10)).
+	CHECK_EQUAL(mytree.size(),2).
       CHECK_OK(mytree.insert(326326)).
+	CHECK_EQUAL(mytree.size(),3).
       CHECK_OK(mytree.insert(-467347)).
+	CHECK_EQUAL(mytree.size(),4).
       CHECK_OK(mytree.insert(0)).
+	CHECK_EQUAL(mytree.size(),5).
       CHECK_OK(mytree.insert(1)).
+	CHECK_EQUAL(mytree.size(),6).
       CHECK_THROWS(mytree.left(1)).
       CHECK_EQUAL(mytree.right(0) , 1).
       CHECK_EQUAL(mytree.contains(0) , true).
       CHECK_OK(mytree.remove(0)).
       CHECK_THROWS(mytree.remove(0)).
+      CHECK_OK(mytree.remove(-2)).
       print();
+
+
+	tc.CHECK_OK(bigtree.insert(20)).
+	CHECK_EQUAL(bigtree.root(),20).
+	CHECK_EQUAL(bigtree.size(),1).
+	CHECK_OK(bigtree.insert(30)).
+	CHECK_EQUAL(bigtree.size(),2).
+	CHECK_OK(bigtree.insert(40)).
+	CHECK_EQUAL(bigtree.size(),3).
+	CHECK_OK(bigtree.insert(60)).
+	CHECK_EQUAL(bigtree.size(),4).
+	CHECK_OK(bigtree.insert(70)).
+	CHECK_EQUAL(bigtree.size(),5).
+	CHECK_OK(bigtree.insert(35)).
+	CHECK_EQUAL(bigtree.size(),6).
+	CHECK_OK(bigtree.insert(44)).
+	CHECK_EQUAL(bigtree.size(),7).
+	CHECK_THROWS(bigtree.insert(35)).
+	CHECK_EQUAL(bigtree.size(),7).
+	CHECK_THROWS(bigtree.insert(44)).
+	CHECK_EQUAL(bigtree.size(),7).
+	CHECK_OK(bigtree.remove(70)).
+	CHECK_THROWS(bigtree.remove(70)).
+	CHECK_EQUAL(bigtree.size(),6).
+	CHECK_OK(bigtree.remove(20)).
+	CHECK_EQUAL(bigtree.size(),5).
+	CHECK_THROWS(bigtree.remove(20)).
+	CHECK_OK(bigtree.remove(44)).
+	CHECK_EQUAL(bigtree.size(),4).
+	CHECK_THROWS(bigtree.remove(44)).
+	print();
+
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
 }
